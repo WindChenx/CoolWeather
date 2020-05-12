a4c45f89909e20957d1674f5516f6c1e7d3321d8
wind2048 <190235290@qq.com>
2020-05-12 23:03:21 +0800

    Initial commit

---
+++ app/src/main/java/com/wind/coolweather/db/County.java
@@ -0,0 +1,34 @@
+package com.wind.coolweather.db;
+
+
+import org.litepal.crud.LitePalSupport;
+
+public class County extends LitePalSupport {
+    private int id;
+    private String countyName;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
---
+++ app/src/main/java/com/wind/coolweather/util/Utility.java
@@ -0,0 +1,74 @@
+package com.wind.coolweather.util;
+
+import android.text.TextUtils;
+
+import com.wind.coolweather.db.City;
+import com.wind.coolweather.db.County;
+import com.wind.coolweather.db.Province;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Utility {
+    public static boolean handleProvinceResponse(String response){
+        if(!TextUtils.isEmpty(response)){
+            try{
+                JSONArray allProvinces=new JSONArray(response);
+                for(int i=0;i<allProvinces.length();i++){
+                    JSONObject provinceObject=allProvinces.getJSONObject(i);
+                    Province province=new Province();
+                    province.setProvinceName(provinceObject.getString("name"));
+                    province.setProvinceCode(provinceObject.getInt("id"));
+                    province.save();
+                }
+                return true;
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+
+        }
+        return false;
+    }
+
+    public static boolean handleCityResponse(String response,int provinceId){
+        if(!TextUtils.isEmpty(response)){
+            try{
+                JSONArray allCitys=new JSONArray(response);
+                for(int i=0;i<allCitys.length();i++){
+                    JSONObject cityObject=allCitys.getJSONObject(i);
+                    City city=new City();
+                    city.setCityName(cityObject.getString("name"));
+                    city.setCityCode(cityObject.getInt("id"));
+                    city.setProvinceId(provinceId);
+                    city.save();
+                }
+                return true;
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public static boolean handleCountyResponse(String response,int cityId){
+        if(!TextUtils.isEmpty(response)){
+            try{
+                JSONArray allCounty=new JSONArray(response);
+                for(int i=0;i<allCounty.length();i++){
+                    JSONObject countyObject=allCounty.getJSONObject(i);
+                    County county=new County();
+                    county.setCountyName(countyObject.getString("name"));
+                    county.setId(countyObject.getInt("id"));
+                    county.setCityId(cityId);
+                    county.save();
+
+                }
+                return true;
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+}
